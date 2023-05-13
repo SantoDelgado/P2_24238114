0a8aaa8d684588b0ff6d03f3195ad969c7542f2d
Santo Delgado <eserxzd@gmail.com>
2023-05-13 12:55:17 -0400

    Integración de Sqlite
    Base de datos Sqlite3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const db = require('../database');
 var router = express.Router();
 
-/* GET home page. */
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+  let name = req.body.name;
+  let email = req.body.email;
+  let comment = req.body.comment;
+  let date = new Date().toISOString();
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress 
+  db.insert(name, email, comment, date, ip);
+
+  res.redirect('/');
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
 
 module.exports = router;
